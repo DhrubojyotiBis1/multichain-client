60559bb53953f90b2b95e1a0b34d945fb1b787c3
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2021-01-24 23:45:12 +0530

    some improvement

--- multichain_warpper.js
+++ multichain_warpper.js
@@ -3,19 +3,24 @@
 //const config = require("./config.json")
 
 let multichain = require("multichain-node")({
-    port: 6282,
-    host: '127.0.0.1',
-    user: "multichainrpc",
-    pass: "somepass"
+    port: 6281,
+    host: '120.0.0.1',
+    user: "test",
+    pass: "test"
 });
 
 class Bench {
+
+    async getInfo() {
+        let returnValue = await multichain.getInfo().catch(err => {return Promise.reject(err)});
+        return Promise.resolve(returnValue)
+    };
+
     async sendAssets(to)  {
-        let returnValue = multichain.send({
-            address: to,
-            amount: {
-                "barcoin": 50
-            }
+        let returnValue = multichain.sendAsset({
+        address: to,
+        asset: "barcoin",
+                    qty: 50 
         }).catch(err => {return Promise.reject(err)});
         console.log(returnValue);
         //return Promise.resolve()
--- test.js
+++ test.js
@@ -1,13 +1,14 @@
 'use strict';
-
+  
 const Bench = require("./multichain_warpper.js");
 const bench = new Bench();
 
 async function test(){
 
-    console.log("blockchain: ")
+console.log("blockchain: ")
     console.log(" ")
-    await bench.sendAssets('sdhgjdsksfs') 
+    await bench.getInfo();
+   //:await bench.sendAssets("1aiznfgqUwcaZdTzV9GRtz2DbE16F8os3QMHXT") 
     console.log(" ")
 }
 
