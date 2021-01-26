436cf295a410f7034acd0565edac94c96c6661c1
DhrubojyotiBis1 <bisdhruv1998@gmail.com>
2021-01-27 01:54:26 +0530

    Every function working, test.js working

--- multichain_warpper.js
+++ multichain_warpper.js
@@ -1,30 +1,69 @@
 'use strict';
-
+  
 //const config = require("./config.json")
 
 let multichain = require("multichain-node")({
-    port: 6281,
-    host: '120.0.0.1',
-    user: "test",
-    pass: "test"
+    port: 6280, //port number at which node is Listening for API request
+    host: '127.0.0.1',
+    user: "USER", //should be set at the time of starting node by --rpcuser=USER
+    pass: "PASSWORD" //should be set at the time of starting node by --rpcpassword=PASSWORD
 });
 
 class Bench {
 
-    async getInfo() {
-        let returnValue = await multichain.getInfo().catch(err => {return Promise.reject(err)});
+    async getInfo(){
+        let returnValue =  await multichain.getInfo().catch(err => {return Promise.reject(err)});
         return Promise.resolve(returnValue)
-    };
+    }
 
-    async sendAssets(to)  {
-        let returnValue = multichain.sendAsset({
-        address: to,
-        asset: "barcoin",
-                    qty: 50 
-        }).catch(err => {return Promise.reject(err)});
-        console.log(returnValue);
-        //return Promise.resolve()
-    };
+    async getPeerInfo(){
+        let returnValue = await multichain.getPeerInfo().catch(err => {return Promise.reject(err)});
+        return Promise.resolve(returnValue)
+    }
+
+    async getAddresses(withDetail){
+        let returnValue = await multichain.getAddresses(
+            {verbose: withDetail}
+        ).catch(err => {return Promise.reject(err)});
+        return Promise.resolve(returnValue)
+    }
+
+    async getBlockchainInfo(){
+        let returnValue = await multichain.getBlockchainInfo().catch(err =>{return Promise.reject(err)});
+        return Promise.resolve(returnValue)
+    }
+
+    async grant(addresses, permissions){
+        let permissionsString = permissions.join();
+        let addressesString = addresses.join();
+        let returnValue = await multichain.grant({
+            addresses:addressesString,
+            permissions: permissionsString
+        }).catch(err =>{return Promise.reject(err)});
+        return Promise.resolve(returnValue)
+    }
+
+    async issueAsset(name, address, quantity, unit){
+        let returnValue = await multichain.issue({
+            address: address,
+            asset: {
+            name: name,
+            open: true
+            },
+            qty: quantity,
+            units: unit
+        }).catch(err =>{return Promise.reject(err)});
+        return Promise.resolve(returnValue)
+    }
+
+    async sendAsset(name, address, qty){
+        let returnValue = await multichain.sendAsset({
+            address: address,
+            asset: name,
+            qty: qty
+        }).catch(err =>{return Promise.reject(err)});
+        return Promise.resolve(returnValue)
+    }
 
 }
 
--- test.js
+++ test.js
@@ -5,11 +5,71 @@ const bench = new Bench();
 
 async function test(){
 
-console.log("blockchain: ")
-    console.log(" ")
-    await bench.getInfo();
-   //:await bench.sendAssets("1aiznfgqUwcaZdTzV9GRtz2DbE16F8os3QMHXT") 
-    console.log(" ")
-}
+    console.log("STARTING TEST")
 
+    await bench.getInfo()
+    .then(returnVal => {
+            console.log("getInfo");
+        console.log(returnVal);
+    })
+    console.log("")
+    console.log("=======================")
+    console.log("")
+
+    await bench.getPeerInfo()
+    .then(returnVal => {
+        console.log("getPeerInfo");
+        console.log(returnVal);
+    })
+    console.log("")
+    console.log("=======================")
+    console.log("")
+
+    await bench.getAddresses(false)
+    .then(returnVal => {
+        console.log("getAddresses");
+        console.log(returnVal);
+    })
+    console.log("")
+    console.log("=======================")
+    console.log("")
+
+    await bench.getBlockchainInfo()
+    .then(returnVal => {
+        console.log("getBlockchainInfo");
+        console.log(returnVal);
+    })
+    console.log("")
+    console.log("=======================")
+    console.log("")
+
+    await bench.grant(["addressToGrantPerssion"],["mine","send","receive","issue","admin"])
+    .then(returnVal => {
+        console.log("grant");
+        console.log(returnVal);
+    })
+    console.log("")
+    console.log("=======================")
+    console.log("")
+
+    await bench.issueAsset("nameOfAsset", "addressToIssueAsset", 1000, 0.01)
+    .then(returnVal => {
+        console.log("issueAssets");
+        console.log(returnVal);
+    })
+    console.log("")
+    console.log("=======================")
+    console.log("")
+
+    await bench.sendAsset("nameOfAsset", "addressToSendAsset", 150)
+    .then(returnVal => {
+        console.log("sendAsset");
+        console.log(returnVal);
+    })
+    console.log("")
+    console.log("=======================")
+    console.log("")
+
+}
+        
 test()
